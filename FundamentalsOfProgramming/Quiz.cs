365067364d6cdb1a4bbe0cdb921b24e5c6a6d0c0
Jack <jsim89@gmail.com>
2018-02-10 09:32:06 +0800

    Added answer for Day 5 quiz

--- FundamentalsOfProgramming/Quiz.cs
+++ FundamentalsOfProgramming/Quiz.cs
@@ -95,5 +95,46 @@ namespace Workshop
             }
 
         }
+
+        public static void Q5()
+        {
+            int[,] theArray = new int[,]
+               {
+                { 5, 3 },
+                { 2, 9 },
+                { 2, 9 },
+                { 5, 1 },
+                { 7, 1 },
+                { 8, 3 },
+                { 0, 3 },
+                { 2, 2 },
+                { 9, 7 }
+               };
+
+            int count = 0;
+
+            Console.Write("Please enter a number (0-9): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < theArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < theArray.GetLength(1); j++)
+                {
+                    if (theArray[i,j] == input)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("There is no {0} in the array.", input);
+            }
+            else
+            {
+                Console.WriteLine("The number of {0} is {1}", input, count);
+            }
+        }
     }
 }

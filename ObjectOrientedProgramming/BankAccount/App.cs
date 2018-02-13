cb9342a78263c54f7046ca24ff4659174f6d808c
Jack <jsim89@gmail.com>
2018-02-13 13:19:10 +0800

    Added Bank Account project

---
+++ ObjectOrientedProgramming/BankAccount/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account("001-001-001", "Tan Ah Kow", 2000);
+            Account b = new Account("001-001-002", "Kim Keng Lee", 5000);
+            Console.WriteLine("\nAccount set-up:");
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+
+            Console.WriteLine("\nDeposit and withdrawal from account a");
+            a.Deposit(100);
+            Console.WriteLine(a.Show());
+            a.Withdraw(200);
+            Console.WriteLine(a.Show());
+
+            Console.WriteLine("\nTransfer from a to b");
+            a.TransferTo(300, b);
+            Console.WriteLine(a.Show());
+            Console.WriteLine(b.Show());
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/BankAccount/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Account
+    {
+        private string accountNumber;
+        private string accountHolderName;
+        private double balance;
+
+        public Account(string aN, string aHN, double bal)
+        {
+            accountNumber = aN;
+            accountHolderName = aHN;
+            balance = bal;
+        }
+
+        public string Show()
+        {
+            return String.Format("{0}\t{1}\t{2:C}", accountNumber, accountHolderName, balance);
+        }
+
+        public void Withdraw(double amt)
+        {
+            balance -= amt;
+        }
+
+        public void Deposit(double amt)
+        {
+            balance += amt;
+        }
+
+        public void TransferTo(double amt, Account target)
+        {
+            Withdraw(amt);
+            target.Deposit(amt);
+        }
+    }
+}

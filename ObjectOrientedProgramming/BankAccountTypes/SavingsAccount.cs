2518974a168a0def80600f41a984b0fba673fb7d
Jack <jsim89@gmail.com>
2018-02-19 22:45:48 +0800

    Added SavingsAccount and TestSavingsAccountClass

---
+++ ObjectOrientedProgramming/BankAccountTypes/SavingsAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BankAccountTypes
+{
+    class SavingsAccount : Account
+    {
+        public SavingsAccount(string aN, string aHN, double bal) : base(aN, aHN, bal)
+        {
+            {
+                Deposit(bal);
+            }
+        }
+
+        public new string ToString()
+        {
+            return String.Format(String.Format("SavingsAccount({0}, {1}, {2:C})", accNumber, accHolderName, balance));
+        }
+
+        public new bool Withdraw(double amt)
+        {
+            if (amt > Bal)
+            {
+                Console.WriteLine("There is insufficient funds in your account");
+                return false;
+            }
+            else
+            {
+                base.Withdraw(amt);
+                return true;
+            }
+        }
+
+        public new void TransferTo(double amt, Account targetAcc)
+        {
+            if (Withdraw(amt))
+            {
+                targetAcc.Deposit(amt);
+            }
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/BankAccountTypes/TestSavingsAccount.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BankAccountTypes
+{
+    [TestFixture]
+    class TestSavingsAccount
+    {
+        SavingsAccount acc;
+        SavingsAccount targetAcc;
+        Account parentTargetAcc;
+        Random r = new Random();
+
+        [SetUp]
+        public void SetUp()
+        {
+            acc = new SavingsAccount("Tan", "000-001", 2000);
+            targetAcc = new SavingsAccount("Lim", "000-002", 3000);
+            parentTargetAcc = new Account("Sim", "000-000", 5000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            acc = null;
+            targetAcc = null;
+        }
+
+        //ToString()
+        [TestCase]
+        public void TestTheReturnValueOfToString()
+        {
+            string correctString = String.Format("SavingsAccount({0}, {1}, {2:C})", acc.AccNo, acc.AccHolderName, acc.Bal);
+
+            Assert.AreEqual(acc.ToString(), correctString);
+        }
+
+
+        // Withdraw()
+        [TestCase]
+        public void TestForSuccessfulWithdrawWhenAmtLessThanBalance()
+        {
+            int accBal = (int)acc.Bal;
+            double withdrawAmt = r.Next(1, accBal);
+            double correctBalance = acc.Bal - withdrawAmt;
+
+            acc.Withdraw(withdrawAmt);
+
+            Assert.AreEqual(correctBalance, acc.Bal);
+        }
+
+        [TestCase]
+        public void TestThatSuccessfulWithdrawalReturnsTrue()
+        {
+            int accBal = (int)acc.Bal;
+            double withdrawAmt = r.Next(1, accBal);
+
+            bool rv = acc.Withdraw(withdrawAmt);
+
+            Assert.IsTrue(rv);
+        }
+
+        [TestCase]
+        public void TestForFailedWithdrawalWhenAmtMoreThanBalance()
+        {
+            int accBal = (int)acc.Bal;
+            double withdrawAmt = r.Next(accBal + 1, accBal+1000);
+            double correctBalance = acc.Bal;
+
+            acc.Withdraw(withdrawAmt);
+
+            Assert.AreEqual(correctBalance, acc.Bal);
+        }
+
+        [TestCase]
+        public void TestThatFailedWithdrawalReturnsFalse()
+        {
+            int accBal = (int)acc.Bal;
+            double withdrawAmt = r.Next(accBal + 1, accBal+1000);
+
+            bool rv = acc.Withdraw(withdrawAmt);
+
+            Assert.IsFalse(rv);
+        }
+
+        // Deposit()
+        [TestCase]
+        public void TestThatDepositAddsCorrectAmountToBalance()
+        {
+            double depositAmt = r.NextDouble() * (4999) + 1;
+            double correctBal = acc.Bal + depositAmt;
+
+            acc.Deposit(depositAmt);
+
+            Assert.AreEqual(acc.Bal, correctBal);
+        }
+
+        // TransferTo()
+        [TestCase]
+        public void TestThatTransferWithdrawsFromAccountWhenAmtLessThanBalance()
+        {
+            double transferAmt = r.NextDouble() * (acc.Bal - 1) + 1;
+            double correctAccBal = acc.Bal - transferAmt;
+
+            acc.TransferTo(transferAmt, targetAcc);
+
+            Assert.AreEqual(acc.Bal, correctAccBal);
+        }
+        [TestCase]
+        public void TestThatTransferDepositsToTargetAccountWhenAmtLessThanBalance()
+        {
+            double transferAmt = r.NextDouble() * (acc.Bal - 1) + 1;
+            double correctTargetBal = targetAcc.Bal + transferAmt;
+
+            acc.TransferTo(transferAmt, targetAcc);
+
+            Assert.AreEqual(targetAcc.Bal, correctTargetBal);
+        }
+        
+        [TestCase]
+        public void TestThatTrasferWithdrawalFailsWhenAmtMoreThanBalance()
+        {
+            double transferAmt = r.NextDouble() * 1000 + acc.Bal;
+            double correctAccBal = acc.Bal;
+
+            acc.TransferTo(transferAmt, targetAcc);
+
+            Assert.AreEqual(acc.Bal, correctAccBal);
+        }
+
+        public void TestThatTransferDepositFailsWhenAmtMoreThanBalance()
+        {
+            double transferAmt = r.NextDouble() * 1000 + acc.Bal;
+            double correctTargetBal = targetAcc.Bal;
+
+            acc.TransferTo(transferAmt, targetAcc);
+
+            Assert.AreEqual(targetAcc.Bal, correctTargetBal);
+        }
+
+        //CalculateInterest()
+        [TestCase]
+        public void TestThatInterestAmountIsCorrectlyCalculated()
+        {
+            double correctInterestAmt = acc.Bal * Account.Interest;
+
+            acc.CalculateInterest();
+
+            Assert.AreEqual(acc.InterestAmt, correctInterestAmt);
+        }
+
+        //CreditInterest()
+        [TestCase]
+        public void TestThatInterestAmtIsCredited()
+        {
+            double correctAmountAfterInterestCredit = acc.Bal * (1 + Account.Interest);
+
+            acc.CreditInterest();
+
+            Assert.AreEqual(acc.Bal, correctAmountAfterInterestCredit);
+        }
+    }
+}

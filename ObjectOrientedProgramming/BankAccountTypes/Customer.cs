c4f9cfca237bd0f75cdd940fe8bab6d7b9501f1b
Jack <jsim89@gmail.com>
2018-02-21 15:55:23 +0800

    Added Customer class and tests

---
+++ ObjectOrientedProgramming/BankAccountTypes/Customer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankAccountTypes
+{
+    class Customer
+    {
+        string id;
+        string name;
+        string contactNo;
+        string email;
+        DateTime dob;
+        HashSet<Account> accounts; 
+        
+        public Customer(string id, string name, DateTime dob)
+        {
+            this.id = id;
+            this.name = name;
+            this.dob = dob;
+            accounts = new HashSet<Account>();
+        }
+
+        public Customer(string id, string name, DateTime dob, string contact):
+             this(id, name, dob)           
+        {
+            UpdateEmail(contact);
+            UpdateContactNo(contact);
+        }
+
+        public Customer(string id, string name, DateTime dob, string contactNo, string email): 
+            this(id, name, dob)
+        {
+            UpdateContactNo(contactNo);
+            UpdateEmail(email);
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public DateTime DOB
+        {
+            get
+            {
+                return dob;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Today.Year - dob.Year;
+            }
+        }
+
+        public string ContactNo
+        {
+            get
+            {
+                return contactNo;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+        }
+
+        public HashSet<Account> Accounts
+        {
+            get
+            {
+                return accounts;
+            }
+        }
+
+        public override string ToString()
+        {
+            string email, contactNo;
+            if (Email is null)
+            {
+                email = "no email";
+            }
+            else
+            {
+                email = Email;
+            }
+
+            if (ContactNo is null)
+            {
+                contactNo = "no contact number";
+            }
+            else
+            {
+                contactNo = ContactNo;
+            }
+            return String.Format("Customer({0}, {1}, {2}, {3}, {4})",
+                Id, Name, Age, contactNo, email);
+        }
+
+        public bool UpdateContactNo(string contactNo)
+        {
+            if (ValidateContactNo(contactNo))
+            {
+                this.contactNo = contactNo;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateContactNo(string contactNo)
+        {
+            if (contactNo.Length == 8 && Int32.TryParse(contactNo, out int temp))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateEmail(string email)
+        {
+            if (ValidateEmail(email))
+            {
+                this.email = email;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            bool rv = true;
+
+            String[] splitEmail = email.Split('@');
+            if (splitEmail.Length != 2)
+            {
+                rv = false;
+            }
+            else if (splitEmail[0].Length == 0)
+            {
+                rv = false;
+            }
+            
+            if (rv)
+            {
+                String[] domain = splitEmail[1].Split('.');
+                if (domain.Length != 2)
+                {
+                    rv = false;
+                }
+                else if (domain[0].Length == 0 || domain[1].Length == 0)
+                {
+                    rv = false;
+                }
+            }
+
+            return rv;
+        }
+
+        public bool AddAccount(Account acc)
+        {
+            return accounts.Add(acc);
+        }
+
+        public bool RemoveAccount(Account acc)
+        {
+            int len = accounts.Count;
+            accounts.Remove(acc);
+            if (len == accounts.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool TransferAccount(Account acc, Customer cust)
+        {
+            if (RemoveAccount(acc))
+            {
+                return cust.AddAccount(acc);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/BankAccountTypes/Tests/TestCustomer.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BankAccountTypes.Tests
+{
+    [TestFixture]
+    class TestCustomer
+    {
+        Customer cust;
+        Customer custEmail;
+        Customer custNo;
+        Customer custContact;
+
+        SavingsAccount acc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            cust = new Customer("0001", "Tan", new DateTime(1997, 1, 1));
+            custEmail = new Customer("0001", "Tan", new DateTime(1997, 1, 1), "js@a.com");
+            custNo = new Customer("0001", "Tan", new DateTime(1997, 1, 1), "92345678");
+            custContact = new Customer("0001", "Tan", new DateTime(1997, 1, 1),"92345678" ,"js@a.com");
+
+            acc = new SavingsAccount("1", "Tan", 2000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            cust = null;
+            custEmail = null;
+            custNo = null;
+            custContact = null;
+
+            acc = null;
+        }
+
+        // Basic Constructor
+        [TestCase]
+        public void TestIdInConstructor()
+        {
+            Assert.AreEqual("0001", cust.Id);
+        }        
+
+        [TestCase]
+        public void TestNameInConstructor()
+        {
+            Assert.AreEqual("Tan", cust.Name);
+        }
+
+        [TestCase]
+        public void TestDobInConstructor()
+        {
+            Assert.AreEqual(new DateTime(1997, 1, 1), cust.DOB);
+        }
+
+        [TestCase]
+        public void TestAgeInConstructor()
+        {
+            int correctAge = DateTime.Today.Year - new DateTime(1997, 1, 1).Year;
+
+            Assert.AreEqual(correctAge, cust.Age);
+        }
+
+        [TestCase]
+        public void TestContactNoBasicConstructor()
+        {
+            Assert.IsNull(cust.ContactNo);
+        }
+
+        [TestCase]
+        public void TestEmailInBasicConstructor()
+        {
+            Assert.IsNull(cust.Email);
+        }
+
+
+        // Constructor with one contact field
+        [TestCase]
+        public void TestContactNoInIncompleteContactsConstructor()
+        {
+            Assert.AreEqual("92345678", custNo.ContactNo);
+        }
+
+        [TestCase]
+        public void TestEmailInIncompleteContactsConstructor()
+        {
+            Assert.AreEqual("js@a.com", custEmail.Email);
+        }
+
+        // Constructor with both contact fields
+        [TestCase]
+        public void TestContactNoInFullContactsConstructor()
+        {
+            Assert.AreEqual("92345678", custContact.ContactNo);
+        }
+
+        [TestCase]
+        public void TestEmailFullContactsConstructor()
+        {
+            Assert.AreEqual("js@a.com", custContact.Email);
+        }
+
+        // Age
+        [TestCase]
+        public void TestGetAge()
+        {
+            int correctAge = DateTime.Today.Year - cust.DOB.Year;
+
+            Assert.AreEqual(correctAge, cust.Age);
+        }
+
+        // ToString
+        [TestCase]
+        public void TestToStringWhenNoContacts()
+        {
+            string correctString = String.Format("Customer({0}, {1}, {2}, {3}, {4})",
+                cust.Id, cust.Name, cust.Age, "no contact number", "no email");
+
+            Assert.AreEqual(correctString, cust.ToString());
+        }
+
+        [TestCase]
+        public void TestToStringWhenOnlyEmail()
+        {
+            string correctString = String.Format("Customer({0}, {1}, {2}, {3}, {4})",
+                custEmail.Id, custEmail.Name, custEmail.Age, "no contact number", custEmail.Email);
+
+            Assert.AreEqual(correctString, custEmail.ToString());
+        }
+        
+        [TestCase]
+        public void TestToStringWhenOnlyContactNo()
+        {
+            string correctString = String.Format("Customer({0}, {1}, {2}, {3}, {4})",
+                custNo.Id, custNo.Name, custNo.Age, custNo.ContactNo, "no email");
+
+            Assert.AreEqual(correctString, custNo.ToString());
+        }
+
+        [TestCase]
+        public void TestToStringWithFullContactDetails()
+        {
+            string correctString = String.Format("Customer({0}, {1}, {2}, {3}, {4})",
+                custContact.Id, custContact.Name, custContact.Age, custContact.ContactNo, custContact.Email);
+
+            Assert.AreEqual(correctString, custContact.ToString());
+        }
+
+        // UpdateEmail
+        [TestCase]
+        public void TestReturnTrueIfValidEmailIsUpdated()
+        {
+            string newEmail = "js2@a.com";
+
+            bool rv = cust.UpdateEmail(newEmail);
+
+            Assert.IsTrue(rv);
+        }
+        
+        [TestCase]
+        public void TestThatEmailIsUpdatedIfValid()
+        {
+            string newEmail = "js2@a.com";
+
+            cust.UpdateEmail(newEmail);
+
+            Assert.AreEqual(newEmail, cust.Email);
+        }
+
+        [TestCase]
+        public void TestReturnFalseIfEmailIsInvalid()
+        {
+            string newEmail = "js2@a.c.om";
+
+            bool rv = cust.UpdateEmail(newEmail);
+
+            Assert.IsFalse(rv);
+        }
+        
+        [TestCase]
+        public void TestThatEmailIsNotUpdatedIfInvalid()
+        {
+            string newEmail = "js2@a.c.om";
+
+            cust.UpdateEmail(newEmail); // cust email = null.
+
+            Assert.IsNull(cust.Email);
+        }
+
+        // UpdateContact
+        [TestCase]
+        public void TestReturnTrueIfValidContactIsUpdated()
+        {
+            string newContact = "99887766";
+
+            bool rv = cust.UpdateContactNo(newContact);
+
+            Assert.IsTrue(rv);
+        }
+
+        [TestCase]
+        public void TestThatValidContactNoIsUpdated()
+        {
+            string newContact = "99887766";
+
+            cust.UpdateContactNo(newContact);
+
+            Assert.AreEqual(newContact, cust.ContactNo);
+        }
+
+        [TestCase]
+        public void TestReturnFalseIfInvalidContactNo()
+        {
+            string newContact = "998877";
+
+            bool rv = cust.UpdateContactNo(newContact);
+
+            Assert.IsFalse(rv);
+        }
+
+        [TestCase]
+        public void TestThatInvalidContactNoIsNotUpdated()
+        {
+            string newContact = "998877";
+
+            cust.UpdateContactNo(newContact);
+
+            Assert.IsNull(cust.ContactNo);
+        }
+
+        // AddAccount
+        [TestCase]
+        public void TestReturnTrueWhenNewAccountAdded()
+        {
+            bool rv = cust.AddAccount(acc);
+
+            Assert.IsTrue(rv);
+        }
+
+        [TestCase]
+        public void TestThatNewAccountIsAdded()
+        {
+            cust.AddAccount(acc);
+
+            Assert.IsTrue(cust.Accounts.Contains(acc));
+        }
+
+        [TestCase]
+        public void TestReturnFalseWhenAccountAlreadyExists()
+        {
+            cust.AddAccount(acc);
+            bool rv = cust.AddAccount(acc);
+
+            Assert.IsFalse(rv);
+        }
+
+        [TestCase]
+        public void TestThatAccountExistsAfterDoubleAdding()
+        {
+            cust.AddAccount(acc);
+            cust.AddAccount(acc);
+
+            Assert.IsTrue(cust.Accounts.Contains(acc));
+        }
+
+        // RemoveAccount
+        [TestCase]
+        public void TestReturnFalseWhenNoAccountToRemove()
+        {
+            bool rv = cust.RemoveAccount(acc);
+
+            Assert.IsFalse(rv);
+        }
+
+        [TestCase]
+        public void TestReturnTrueWhenAccountIsRemoved()
+        {
+            cust.AddAccount(acc);
+            bool rv = cust.RemoveAccount(acc);
+
+            Assert.IsTrue(rv);
+        }
+
+        [TestCase]
+        public void TestAccountNoLongerExistsWhenRemoved()
+        {
+            cust.AddAccount(acc);
+            cust.RemoveAccount(acc);
+
+            Assert.IsFalse(cust.Accounts.Contains(acc));
+        }
+
+        // TransferAccount
+        [TestCase]
+        public void TestReturnTrueIfAccountSuccesfullyTransferred()
+        {
+            cust.AddAccount(acc);
+            custEmail.RemoveAccount(acc);
+
+            bool rv = cust.TransferAccount(acc, custEmail);
+
+            Assert.IsTrue(rv);
+        }
+
+        [TestCase]
+        public void TestThatOriginalCustomerNoLongerHasAccountAfterTransfer()
+        {
+            cust.AddAccount(acc);
+            custEmail.RemoveAccount(acc);
+
+            bool rv = cust.TransferAccount(acc, custEmail);
+
+            Assert.IsFalse(cust.Accounts.Contains(acc));
+        }
+ 
+  
+        [TestCase]
+        public void TestThatTargetCustomerHasAccountAfterTransfer()
+        {
+            cust.AddAccount(acc);
+            custEmail.RemoveAccount(acc);
+
+            bool rv = cust.TransferAccount(acc, custEmail);
+
+            Assert.IsTrue(custEmail.Accounts.Contains(acc));
+        }
+
+        [TestCase]
+        public void TestReturnTrueIfAccountAlreadyExistsForCustomer()
+        {
+            cust.AddAccount(acc);
+            custEmail.AddAccount(acc);
+
+            bool rv = cust.TransferAccount(acc, custEmail);
+
+            Assert.IsFalse(rv);
+        }
+
+        [TestCase]
+        public void TestReturnTrueIfFailsAsCustomerDoesNotOwnAccount()
+        {
+            bool rv = cust.TransferAccount(acc, custEmail);
+
+            Assert.IsFalse(rv);
+        }
+    }
+}

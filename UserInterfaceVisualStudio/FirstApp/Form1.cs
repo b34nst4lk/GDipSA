3393b706d7d965854d169c500f40041aec5e3eb9
Jack <jsim89@gmail.com>
2018-02-27 23:07:04 +0800

    Added FirstWorkshopApp, which contains a bundle of nonsense and experimentation

---
+++ UserInterfaceVisualStudio/FirstApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("IT'S ALIVE!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The temptation is too strong! We have to do it!");
+            Form2 f = new Form2();
+            f.PassForwardAndShow(this);
+            Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Come on dude, cut me some slack. Fine! you clicked on " + button3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string test = String.Format("Time to see how this works:\n-{0}\n{1}\n{2}", button1.Text, button2.RightToLeft, Text);
+            MessageBox.Show(test);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ UserInterfaceVisualStudio/FirstApp/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    public partial class Form2 : Form
+    {
+        Form1 previousForm;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void PassForwardAndShow(Form1 f)
+        {
+            previousForm = f;
+            Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            previousForm.Show();
+            Hide();
+        }
+    }
+}
